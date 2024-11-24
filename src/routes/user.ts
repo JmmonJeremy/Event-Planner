13ee95e6727f3f279c25115ec0b41147c0de5e6d
Daniel Hinojos <daniel.hinojos@kroniatech.com>
2024-11-23 21:16:59 -0600

    mongodb connection completed

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,83 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import bcrypt from 'bcryptjs';
+import UserModel from '../models/userModel';
 
 const userRoutes = Router();
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Create a new user
+ *     description: Creates a new user in the system
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the user
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The email of the user
+ *               password:
+ *                 type: string
+ *                 description: The password for the user
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: Successfully created the user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ *       400:
+ *         description: Bad request if validation fails
+ */
+userRoutes.post('/user', async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
 
-userRoutes.post('/user', (req, res) => {
-    res.send('Create a user');
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json({
+      name: savedUser.name,
+      email: savedUser.email,
+      _id: savedUser._id,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(400).json({ message: 'An unknown error occurred' });
+    }
+  }
 });
+
+
+
+
 
 userRoutes.put('/user/:userId', (req, res) => {
     res.send(`Update user with ID ${req.params.userId}`);
