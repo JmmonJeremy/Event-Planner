91fb12ae460d8c96be455314429cab622597f91a
JmmonJeremy <suc19002@byui.edu>
2024-11-30 09:06:43 -0800

    Added GET Method for all users and organized order of Routes and Methods

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -2,6 +2,45 @@ import { Request, Response, Router } from 'express';
 import bcrypt from 'bcryptjs';
 import UserModel from '../models/userModel';
 
+// START Extra CRUD Operation Methods #####################################################################################/
+/*** EXTRA types of GET METHODS *******************************************************************************************/
+// #1 extra "Get" METHOD to login USER
+
+// END Extra CRUD Operation Methods #######################################################################################/
+
+// START Basic CRUD Operation Methods #####################################################################################/
+/*** MAIN 2 types of GET METHODS ******************************************************************************************/
+// #1 main "Get" METHOD for getting all USERS
+export const findAll = async (req: Request, res: Response): Promise<void> => {
+/* #swagger.summary = "GETS all the users" */   
+/* #swagger.description = 'All users are displayed.' */    
+// #swagger.responses[200] = { description: 'SUCCESS, GET retrieved all users' }   
+// #swagger.responses[404] = { description: 'The attempted GET of all users was Not Found'}
+// #swagger.responses[500] = { description: 'There was an INTERNAL SERVER ERROR while trying to GET all users'}
+  try {
+    console.log(UserModel)
+    const data = await UserModel.find({}).sort({ lastName: 1 });
+    if (!data || data.length === 0) {
+      // BACKEND OUTPUT   
+      res.status(404).send({ 
+        message: 'No users found! There are either no users yet, or their was an error retrieving them.'
+      }); 
+      return;
+    } else {
+      res.status(200).send(data); // Send the newly ordered data  
+      }
+  } catch(err: unknown) {
+    const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+    res.status(500).send({
+      message: errorMessage,
+    });  
+  }
+}
+
+// #2 main "Get" method for getting 1 USER by id
+
+/*** MAIN 3 alter data METHODS ********************************************************************************************/
+// #1 the "Post" METHOD for a new USER
 export const create = async (req: Request, res: Response) => { 
   const { name, email, password } = req.body;
 
@@ -29,3 +68,4 @@ export const create = async (req: Request, res: Response) => {
     }
   }
 };
+// END Basic CRUD Operation Methods #######################################################################################/
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,37 +1,51 @@
 import { Request, Response, Router } from 'express';
 import * as userController from '../controllers/userControllers';
-import bcrypt from 'bcryptjs';
-import UserModel from '../models/userModel';
 
 const userRoutes = Router();
+
+// START Extra CRUD Operation Routes ######################################################################################/
+/*** EXTRA types of GET ROUTES ********************************************************************************************/
+// #1 extra "Get" ROUTE to login USER
+userRoutes.get('/user/login', (req, res) => {
+  // #swagger.ignore = true
+  /* #swagger.summary = "Extra GET Route for OAUTH Login" */   
+  res.send('User login');
+});
+
+userRoutes.get('/user/logout', (req, res) => {
+  // #swagger.ignore = true
+  /* #swagger.summary = "Extra GET Route for OAUTH Logout" */  
+  res.send('User logout');
+});
+
+userRoutes.get('/user/findByGoogleId', (req, res) => {
+  // #swagger.ignore = true
+  /* #swagger.summary = "Extra GET Route for Google ID" */  
+  res.send('Find user by Google ID');
+});
+// END Extra CRUD Operation Routes ########################################################################################/
+
+// START Basic CRUD Operation Routes ######################################################################################/
+/*** MAIN 2 types of GET METHODS ******************************************************************************************/
+// #1 main "Get" ROUTE for getting all USERS
+userRoutes.get('/users', userController.findAll)
+
+// #2 main "Get" ROUTE for getting 1 USER by id
+userRoutes.get('/user/:userId', (req, res) => {
+  res.send(`Get user with ID ${req.params.userId}`);
+});
+
+/*** MAIN 3 alter data ROUTES ********************************************************************************************/
+// #1 the "Post" ROUTE for a new USER
 userRoutes.post('/user', userController.create);
-
-
-
-
 
 userRoutes.put('/user/:userId', (req, res) => {
     res.send(`Update user with ID ${req.params.userId}`);
 });
 
-userRoutes.get('/user/login', (req, res) => {
-    res.send('User login');
-});
-
-userRoutes.get('/user/logout', (req, res) => {
-    res.send('User logout');
-});
-
-userRoutes.get('/user/findByGoogleId', (req, res) => {
-    res.send('Find user by Google ID');
-});
-
-userRoutes.get('/user/:userId', (req, res) => {
-    res.send(`Get user with ID ${req.params.userId}`);
-});
-
 userRoutes.delete('/user/:userId', (req, res) => {
-    res.send(`Delete user with ID ${req.params.userId}`);
+  res.send(`Delete user with ID ${req.params.userId}`);
 });
+// END Basic CRUD Operation Routes ########################################################################################/
 
 export default userRoutes;
