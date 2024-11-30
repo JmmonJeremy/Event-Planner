aeebee1db16bf24e7620d4e1ef6cfb9811ce80ec
Ben Davis <bendavis493@gmail.com>
2024-11-30 10:54:11 -0700

    basic crud operations for classes. Needs validation and error handling still

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,29 +1,49 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import ClassModel from '../models/classModel';
 
 const classRoutes = Router();
 
-classRoutes.post('/classes', (req, res) => {
-    res.send('Create a new class');
+classRoutes.post('/classes', async (req: Request, res: Response) => {
+        /* #swagger.parameters['reqBody'] = {
+      in: "body",
+      description: "request body",
+      type: "object",
+      required: true
+} */
+    const newClass = new ClassModel(req.body);
+    const result = await newClass.save();
+    res.status(201).json({_id: result._id})
 });
 
-classRoutes.post('/classes/createWithArray', (req, res) => {
-    res.send('Create multiple classes');
+classRoutes.post('/classes/createWithArray', async (req: Request, res: Response) => {
+    const result = await ClassModel.insertMany((req.body as Array<Object>));
+    res.status(201).json(result);
 });
 
-classRoutes.get('/classes/:classId', (req, res) => {
-    res.send(`Get class with ID ${req.params.classId}`);
+classRoutes.get('/classes/:classId', async (req: Request, res: Response) => {
+    const foundClass = await ClassModel.findById(req.params.classId);
+    res.status(200).json(foundClass);
 });
 
-classRoutes.get('/classes/user/:userId', (req, res) => {
-    res.send(`Get classes for user ${req.params.userId}`);
+classRoutes.get('/classes/user/:userId', async (req: Request, res: Response) => {
+    const foundClass = await ClassModel.findOne({"userId":req.params.userId});
+    res.status(200).json(foundClass);
 });
 
-classRoutes.put('/classes/:classId', (req, res) => {
-    res.send(`Update class with ID ${req.params.classId}`);
+classRoutes.put('/classes/:classId', async (req: Request, res: Response) => {
+        /* #swagger.parameters['reqBody'] = {
+      in: "body",
+      description: "request body",
+      type: "object",
+      required: true
+} */
+    const result = await ClassModel.updateOne({_id:req.params.classId}, req.body);
+    res.status(204).json(result);
 });
 
-classRoutes.delete('/classes/:classId', (req, res) => {
-    res.send(`Delete class with ID ${req.params.classId}`);
+classRoutes.delete('/classes/:classId', async (req: Request, res: Response) => {
+    const result = await ClassModel.deleteOne({_id:req.params.classId});
+    res.status(200).json(result);
 });
 
 export default classRoutes;
